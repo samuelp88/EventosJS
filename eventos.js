cfc04c26f715b57a74e8140c3ad3255aa6843c4f
samuelp88 <41595779+samuelp88@users.noreply.github.com>
2021-03-07 21:28:16 -0300

    commit inicial

---
+++ eventos.js
@@ -0,0 +1,33 @@
+class Events {
+    constructor() {
+        // Propriedades Privadas
+        var listenedEvents = [];
+        
+        // Metodos Publicos
+          
+        this.on = function(eventName, fn) {
+            listenedEvents.push(EventConstructor(eventName, fn));
+            this.listenedEvents = listenedEvents;
+        }
+        this.emmit = function(eventName, eventData) {
+            listenedEvents.filter((value) => {
+                return value.name === eventName;
+            }).forEach((value) => {
+                value.fn(eventData);
+            })
+            
+        }
+
+        // Metodos Privados
+
+        // Desnecessario mas facilita na leitura do código
+        var EventConstructor = function(eventName, fn){
+            var event = {}
+            event.name = eventName;
+            event.fn = fn
+            return event;
+        }
+    }
+}
+
+
